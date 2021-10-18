db9427c7697b860db97d8d22760f65d6af265524
Ba-Ku <90610126+Ba-Ku@users.noreply.github.com>
2021-10-18 22:05:50 +0200

    Built appstructure
    added functionality to component and a directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,27 +5,46 @@ import {Component} from '@angular/core';
   template: `
     <h3>{{title}}</h3>
     <label>Enter your height please:
-      <input [(ngModel)]="height"> cm
+      <input [(ngModel)]="height" (input)="clearBMI()" appMandatory="2"> cm
     </label>
     <br>
     <label>Enter your weight please:
-      <input [(ngModel)]="weight"> cm
+      <input [(ngModel)]="weight" (input)="clearBMI()" appMandatory="2"> cm
     </label>
     <br><br>
-    <button (click)="calculateBMI()">Calculate BMI</button>
+    <button (click)="calculateBMI()" alt="a button to calculate the bmi.">Calculate BMI</button>
     <br><br>
-    <div>Your BMI is: {{bmi}}</div>
+    <div *ngIf="bmi">
+      <h2>Your BMI is: {{bmi | number: '.2-2'}}</h2>
+      <img [src]="picture" [alt]="altText">
+    </div>
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'Welcome! Calculate your BodyMassIndex.';
-  height = 0;
-  weight = 0;
-  bmi = 0;
+  title: string = 'Welcome! Calculate your BodyMassIndex.';
+  height: number = 0;
+  weight: number = 0;
+  bmi: number = 0;
+  picture: string;
+  altText: string;
 
   calculateBMI(): void {
     let newBmi: number = this.weight / (this.height / 100) ** 2;
     this.bmi = newBmi;
+    if (this.bmi <= 20) {
+      this.picture = "assets/giraffe.jpg";
+      this.altText='A picture showing a giraffe.';
+    } else if (this.bmi <= 26) {
+      this.picture = "assets/horse.jpg";
+      this.altText='A picture showing a horse.';
+    } else {
+      this.picture = "assets/elephant.jpg";
+      this.altText='A picture showing an elephant.';
+    }
+  }
+
+  clearBMI(): void {
+    this.bmi = 0;
   }
 }
--- src/app/mandatory.directive.ts
+++ src/app/mandatory.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import {Directive, HostBinding, HostListener, Input} from '@angular/core';
 
 @Directive({
   selector: '[appMandatory]'
 })
 export class MandatoryDirective {
 
-  constructor() { }
+  constructor() {
+  }
+
+  @Input('appMandatory') minLengthForHeightOrWeightInput = 2;
+
+  @HostBinding('style.backgroundColor') inputBackground: string;
+
+  @HostListener('blur', ['$event'])
+  onBlur(event: FocusEvent) {
+    const inputElement = event.target as HTMLInputElement;
+    const inputElementLength = inputElement.value.trim().length;
+    if (inputElementLength < this.minLengthForHeightOrWeightInput) {
+      this.inputBackground = "red";
+    } else {
+      this.inputBackground = null;
+    }
+  }
 
 }
