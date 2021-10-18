1425335d86d13ec1f7847aeb7d976f1d4818ef2e
Ba-Ku <90610126+Ba-Ku@users.noreply.github.com>
2021-10-18 12:45:42 +0200

    Built appstructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MandatoryDirective } from './mandatory.directive';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MandatoryDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
